7acea4878a99d5e0612741a91e414dbf4cdd36c0
SiyaDetrojag <siyadetroja22@gmail.com>
2026-03-21 22:46:20 +0530

    comma added

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://docbaseragapp.vercel.app"
-]
+ ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
