2d1aeb02912140e3e7b508f2611d7f8fa27c902e
SiyaDetrojag <siyadetroja22@gmail.com>
2026-03-22 11:50:10 +0530

    database url updated

--- backend/database.py
+++ backend/database.py
@@ -14,7 +14,11 @@ engine = None
 SessionLocal = None
 
 if DATABASE_URL:
-    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True,
+        connect_args={"sslmode": "require"}
+    )
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
