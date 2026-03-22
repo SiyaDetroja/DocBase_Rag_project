a5dc59fb3ed0b401b23ef76d7d28f68c72d0502e
SiyaDetrojag <siyadetroja22@gmail.com>
2026-03-22 11:03:21 +0530

    origin add

--- backend/main.py
+++ backend/main.py
@@ -32,8 +32,8 @@ app = FastAPI(title="RAG Web App")
 app.add_middleware(
     CORSMiddleware,
    allow_origins=[
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
+    # "http://127.0.0.1:5500",
+    # "http://localhost:5500",
     "https://docbaseragapp.vercel.app"
  ],
     allow_credentials=True,
