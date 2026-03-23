76fffcdb05bd456b609333d532b219117fabd38e
SiyaDetrojag <siyadetroja22@gmail.com>
2026-03-23 09:57:06 +0530

    fix cors issue

--- backend/main.py
+++ backend/main.py
@@ -32,11 +32,8 @@ app = FastAPI(title="RAG Web App")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://docbaseragapp.vercel.app",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
+        "https://docbaseragapp.vercel.app"
     ],
-    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
