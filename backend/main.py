2027db53b994b1cc1fd9a0ab268b51dc910f271a
SiyaDetrojag <siyadetroja22@gmail.com>
2026-03-24 12:09:02 +0530

    comma added

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ app = FastAPI(title="RAG Web App")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*"
+        "*",
         "https://docbaseragapp.vercel.app",
         
     ],
